40f342538beb026c2642986c3a9f92122dfb2749
Peter Siemens <peterjsiemens@gmail.com>
2022-08-10 16:29:21 -0700

    Update OnChainCollection.test.ts

--- src/lib/collections/OnChainCollection.test.ts
+++ src/lib/collections/OnChainCollection.test.ts
@@ -9,31 +9,12 @@ describe('OnChainCollection', () => {
   const collection = new OnChainCollection({
     config,
     name: randomContractName(),
-
-    schema: metadata.createSchema({
-      fields: {
-        name: metadata.String(),
-        description: metadata.String(),
-        thumbnail: metadata.IPFSFile(),
-        video: metadata.HTTPFile(),
-      },
-      views: (fields: metadata.Fields) => [
-        metadata.DisplayView({
-          name: fields.name,
-          description: fields.description,
-          thumbnail: fields.thumbnail,
-        }),
-        metadata.MediaView({
-          file: fields.video,
-          mediaType: 'video/mp4'
-        })
-      ],
-    }),
+    schema: metadata.defaultSchema,
     owner: ownerAuthorizer,
   });
 
   it('should generate a contract', async () => {
-    console.log(await collection.getContract());
+    await collection.getContract();
   });
 
   it('should deploy a contract', async () => {
@@ -46,19 +27,16 @@ describe('OnChainCollection', () => {
         name: 'NFT 1',
         description: 'This is the first NFT.',
         thumbnail: 'nft-1.jpeg',
-        image: 'nft-1.jpeg',
       },
       {
         name: 'NFT 2',
         description: 'This is the second NFT.',
         thumbnail: 'nft-2.jpeg',
-        image: 'nft-1.jpeg',
       },
       {
         name: 'NFT 3',
         description: 'This is the third NFT.',
         thumbnail: 'nft-3.jpeg',
-        image: 'nft-1.jpeg',
       },
     ];
 
