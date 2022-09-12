916cfbff7b435e4253ca5165aa5ab02c9f201c2f
Peter Siemens <peterjsiemens@gmail.com>
2022-09-12 08:45:28 -0700

    Add additional test cases for allowlist

--- src/lib/contracts/StandardNFTContract.test.ts
+++ src/lib/contracts/StandardNFTContract.test.ts
@@ -50,12 +50,50 @@ describe('StandardNFTContract', () => {
 
   const sale = new ClaimSaleContract(contract);
 
-  it('should start a sale', async () => {
-    await client.send(sale.start({ id: 'default', price: '10.0' }));
+  const allowlistName = 'default';
+
+  it('should be able to add an address we do not control to the allowlist', async () => {
+    await client.send(
+      sale.addToAllowlist({
+        name: allowlistName,
+        addresses: ['0x0ae53cb6e3f42a79'],
+        claims: 1, // this address will be allowed to claim 5 NFTs
+      }),
+    );
   });
 
-  it('should claim an NFT', async () => {
+  it('should start a sale with an allowlist', async () => {
+    await client.send(sale.start({ id: 'default', price: '10.0', allowlist: allowlistName }));
+  });
+
+  it('should not be able to claim an NFT when not on the allowlist', async () => {
+    await expect(async () => {
+      await client.send(sale.claimNFT(ownerAuthorizer.address, ownerAuthorizer, 'default'));
+    }).rejects.toThrow();
+  });
+
+  it('should be able to add our address to the allowlist', async () => {
+    await client.send(
+      sale.addToAllowlist({
+        name: allowlistName,
+        addresses: [ownerAuthorizer.address],
+        claims: 2, // our address will be allowed to claim 5 NFTs
+      }),
+    );
+  });
+
+  it('should be able to claim two NFTs when on the allowlist', async () => {
+    // Claim the first NFT
     await client.send(sale.claimNFT(ownerAuthorizer.address, ownerAuthorizer, 'default'));
+
+    // Claim the second NFT
+    await client.send(sale.claimNFT(ownerAuthorizer.address, ownerAuthorizer, 'default'));
+  });
+
+  it('should not be able to claim a third NFT', async () => {
+    await expect(async () => {
+      await client.send(sale.claimNFT(ownerAuthorizer.address, ownerAuthorizer, 'default'));
+    }).rejects.toThrow();
   });
 
   it('should stop a sale', async () => {
