d1e4fdca521f9628c98797d8ba4d696c3ae845d6
Peter Siemens <peterjsiemens@gmail.com>
2022-09-27 10:41:00 -0700

    Add royalty getter and setter templates

--- src/lib/generators/CommonNFTGenerator.ts
+++ src/lib/generators/CommonNFTGenerator.ts
@@ -1,5 +1,11 @@
+import { registerPartial } from '.';
 import { ContractImports } from '../config';
 import TemplateGenerator from './TemplateGenerator';
+
+// Register the royalties partials
+registerPartial('royaltiesFields', '../../../cadence/common-nft/partials/royalties-fields.partial.cdc')
+registerPartial('royaltiesAdmin', '../../../cadence/common-nft/partials/royalties-admin.partial.cdc')
+registerPartial('royaltiesInit', '../../../cadence/common-nft/partials/royalties-init.partial.cdc')
 
 export class CommonNFTGenerator extends TemplateGenerator {
   static getNFT({
@@ -13,5 +19,36 @@ export class CommonNFTGenerator extends TemplateGenerator {
   }): string {
     return this.generate('../../../cadence/common-nft/scripts/get_nft.template.cdc', { imports, contractName, contractAddress });
   }
+
+  static setRoyalties({
+    imports,
+    contractName,
+    contractAddress,
+  }: {
+    imports: ContractImports;
+    contractName: string;
+    contractAddress: string;
+  }): string {
+    return this.generate('../../../cadence/common-nft/transactions/set_royalties.template.cdc', {
+      imports,
+      contractName,
+      contractAddress,
+    });
+  }
+
+  static getRoyalties({
+    imports,
+    contractName,
+    contractAddress,
+  }: {
+    imports: ContractImports;
+    contractName: string;
+    contractAddress: string;
+  }): string {
+    return this.generate('../../../cadence/common-nft/scripts/get_royalties.template.cdc', {
+      imports,
+      contractName,
+      contractAddress,
+    });
   }
 }
