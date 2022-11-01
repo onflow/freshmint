e789001e2d74b460ec59df2d3ae355001e041b0d
Peter Siemens <peterjsiemens@gmail.com>
2022-11-01 16:26:42 -0700

    Deploy FreshmintEncoding to testnet

--- packages/core/config.ts
+++ packages/core/config.ts
@@ -30,6 +30,7 @@ export const TESTNET: FreshmintConfig = {
     FungibleToken: '0x9a0766d93b6608b7',
     NonFungibleToken: '0x631e88ae7f1d7c20',
     MetadataViews: '0x631e88ae7f1d7c20',
+    FreshmintEncoding: '0x8ab7897dd9d69819',
     FreshmintMetadataViews: '0xc270e330615c6fa0',
     FreshmintClaimSale: '0x2d3d6874bc231156',
     FreshmintLockBox: '0x0cad7d1c09a3a433',
