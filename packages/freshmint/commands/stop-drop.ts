799aca95ccca349fe4a6112fac84863d7f371a72
Peter Siemens <peterjsiemens@gmail.com>
2022-11-22 14:18:05 -0800

    Update stop-drop.ts

--- packages/freshmint/commands/stop-drop.ts
+++ packages/freshmint/commands/stop-drop.ts
@@ -4,7 +4,6 @@ import ora from 'ora';
 import { FlowGateway, FlowNetwork } from '../flow';
 
 export default new Command('stop-drop')
-  .command('stop-drop')
   .description('stop the current drop')
   .option('-n, --network <network>', "Network to use. Either 'emulator', 'testnet' or 'mainnet'", 'emulator')
   .action(stopDrop);
